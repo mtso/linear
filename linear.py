4deb18ce1307c09fa77dfe7ce940cfe86ccfa8bc
mtso <mtsobot@gmail.com>
2014-08-25 00:26:22 -0700

    initial commit

---
+++ linear.py
@@ -0,0 +1,24 @@
+# define a tile object (1 dimension)
+## properties: coordinate (integer), has_key,
+
+# define a factory method
+
+# define commands
+## jump to coordinate
+## if someone already exists at coordinate.. can they see?
+## begin at 0
+
+# user system
+## create_user: writes info to a text file
+## account contains current index
+
+## communication: how?
+
+## currency?
+## how to gain "energy" to move?
+
+
+def factory():
+	space1 = {
+		'coordinate': 0
+	}
